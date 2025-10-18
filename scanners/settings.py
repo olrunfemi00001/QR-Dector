3090b0e892cd1680e24776ef5cea3ab9b6dda51a
olrunfemi00001 <olorunfemiolayinka6@gmail.com>
2025-10-18 19:38:16 +0100

    first commit

--- scanners/settings.py
+++ scanners/settings.py
@@ -52,10 +52,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'scan',
     'drf_yasg',
+    "corsheaders",
+    'whitenoise',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- scanners/urls.py
+++ scanners/urls.py
@@ -44,4 +44,5 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
